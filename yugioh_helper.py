0611f93cb347c49bf8d34ee92f25d0587a60dda3
Raffaele Talente <talente63@gmail.com>
2023-04-16 03:34:42 +0200

    Added sentence generator, added config, cleaned stuff

--- yugioh_helper.py
+++ yugioh_helper.py
@@ -1,27 +1,22 @@
 import logging
-import os
-import json
 from telegram.ext import (
     Application,
     MessageHandler,
     filters
 )
-from dotenv import load_dotenv
 from src.handler import handler
+from src import config as cfg
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-load_dotenv()
-bot_token = os.getenv("bot_token")
-
 def main():
-    application = Application.builder().token(bot_token).build()
+    application = Application.builder().token(cfg.bot_token).build()
     print("Bot started")
     application.add_handler(MessageHandler(filters.COMMAND, handler.on_command))
-    application.add_handler(MessageHandler(filters.TEXT, handler.get_user_id))
-    application.run_polling()
+    application.add_handler(MessageHandler(filters.TEXT, handler.on_user_msg))
+    application.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
